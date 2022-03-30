08b1ee1b5518e7ebfb37d1958a88960debc61f58
NoCT-MGD <85911834+NoCT-MGD@users.noreply.github.com>
2022-03-30 14:21:39 +0900

    商品一覧表示機能＋rubocop修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
   def index
+    @items = Item.all
   end
 
   def new
@@ -17,11 +18,10 @@ class ItemsController < ApplicationController
     end
   end
 
-
-
   private
-  def item_params
-    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_cost_payer_id, :prefecture_id, :shipping_days_id, :price, :image).merge(user_id: current_user.id)
-  end
 
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_cost_payer_id, :prefecture_id,
+                                 :shipping_days_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -64,19 +64,19 @@ RSpec.describe Item, type: :model do
       it 'priceが300未満では出品できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
 
       it 'priceが9,999,999超過では出品できない' do
-        @item.price = 10000000
+        @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
 
       it 'priceが全角では出品できない' do
         @item.price = '３００'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not a number")
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
 
       it 'imageが空では出品できない' do
@@ -88,7 +88,7 @@ RSpec.describe Item, type: :model do
       it 'ユーザーが紐付いていなければ投稿できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User must exist")
+        expect(@item.errors.full_messages).to include('User must exist')
       end
     end
   end
